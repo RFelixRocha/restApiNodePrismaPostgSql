aceec11e00d0fa9e5bff6caa39dfe3f9ccbff283
RFelixRocha <felixrochaeso@gmail.com>
2021-08-12 20:45:33 -0400

    Add main.ts

---
+++ src/main.ts
@@ -0,0 +1,9 @@
+import { PrismaClient } from ".prisma/client";
+const prisma = new PrismaClient()
+async function main() {
+
+}
+
+main()
+      .catch( (e) => console.log(e))
+      .finally( async() => await prisma.disconnect())
